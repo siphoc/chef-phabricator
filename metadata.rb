cc5568c4511d2129b9cc530f3a76c414068f768f
Elan Ruusamäe <glen@delfi.ee>
2013-10-17 00:12:21 +0300

    add metadata.rb

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'phabricator'
+maintainer       'Jelmer Snoeck'
+maintainer_email 'jelmer.snoeck@gmail.com'
+license          'BSD'
+description      'Chef cookbook to install Phabricator'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
